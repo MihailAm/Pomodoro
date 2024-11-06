2736e5100de330080219014d03d48f718d3f7fba
MihailAm <amosov.mihail2280@gmail.com>
2024-11-06 13:45:55 +0300

    feature: (Integration, Tests) write integrations tests

--- tests/fixtures/settings.py
+++ tests/fixtures/settings.py
@@ -1,7 +1,45 @@
-import pytest
+import asyncio
 
+import pytest
+import pytest_asyncio
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+
+from app.infrastructure.database import Base
 from app.settings import Settings
+
 
 @pytest.fixture
 def settings():
     return Settings()
+
+
+engine = create_async_engine(url='postgresql+asyncpg://postgres:1234@localhost:5433/pomodoro-test', future=True,
+                             echo=True, pool_pre_ping=True)
+
+AsyncSessionFactory = async_sessionmaker(
+    engine,
+    autoflush=False,
+    expire_on_commit=False
+)
+
+
+@pytest.fixture(scope="session")
+def event_loop():
+    policy = asyncio.get_event_loop_policy()
+    loop = policy.new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest_asyncio.fixture(scope="session", autouse=True)
+async def init_model(event_loop):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    async with engine.begin() as cleanup_conn:
+        await cleanup_conn.run_sync(Base.metadata.drop_all)
+
+
+@pytest_asyncio.fixture(scope="function")
+async def get_db_session() -> AsyncSession:
+    return AsyncSessionFactory()
--- tests/unit/auth/test_auth_service.py
+++ tests/unit/auth/test_auth_service.py
@@ -1,4 +1,5 @@
 # import pytest
+import pytest
 from jose import jwt
 import datetime as dt
 
@@ -8,27 +9,27 @@ from app.settings import Settings
 from tests.fixtures.settings import settings
 
 
-# pytestmark = pytest.mark.asyncio
+pytestmark = pytest.mark.asyncio
 
 
-async def test_get_google_redirect_url_success(auth_service: AuthService, settings: Settings):
+async def test_get_google_redirect_url_success(auth_service_mock: AuthService, settings: Settings):
     settings_google_redirect_url = settings.google_redirect_url
 
-    auth_service_google_redirect_url = auth_service.get_google_redirect_url()
+    auth_service_google_redirect_url = auth_service_mock.get_google_redirect_url()
 
     assert settings_google_redirect_url == auth_service_google_redirect_url
 
 
-async def test_get_yandex_redirect_url_success(auth_service: AuthService, settings: Settings):
+async def test_get_yandex_redirect_url_success(auth_service_mock: AuthService, settings: Settings):
     settings_yandex_redirect_url = settings.yandex_redirect_url
-    auth_service_yandex_redirect_url = auth_service.get_yandex_redirect_url()
+    auth_service_yandex_redirect_url = auth_service_mock.get_yandex_redirect_url()
     assert settings_yandex_redirect_url == auth_service_yandex_redirect_url
 
 
-async def test_generate_access_token__success(auth_service: AuthService, settings: Settings):
+async def test_generate_access_token__success(auth_service_mock: AuthService, settings: Settings):
     user_id = 1
 
-    access_token = auth_service.generate_access_token(user_id=user_id)
+    access_token = auth_service_mock.generate_access_token(user_id=user_id)
     decoded_access_token = jwt.decode(access_token,
                                       settings.JWT_SECRET_KEY,
                                       algorithms=[settings.JWT_ENCODE_ALGORITHM])
@@ -39,25 +40,25 @@ async def test_generate_access_token__success(auth_service: AuthService, setting
     assert decoded_user_id == user_id
 
 
-async def test_get_user_id_from_access_token__success(auth_service: AuthService):
+async def test_get_user_id_from_access_token__success(auth_service_mock: AuthService):
     user_id = 1
 
-    access_token = auth_service.generate_access_token(user_id=user_id)
-    decoded_user_id = auth_service.get_user_id_from_access_token(access_token=access_token)
+    access_token = auth_service_mock.generate_access_token(user_id=user_id)
+    decoded_user_id = auth_service_mock.get_user_id_from_access_token(access_token=access_token)
 
     assert decoded_user_id == user_id
 
 
-async def test_google_auth_success(auth_service: AuthService):
-    user = await auth_service.google_auth(code="fake_code")
-    decoded_user_id = auth_service.get_user_id_from_access_token(access_token=user.access_token)
+async def test_google_auth_success(auth_service_mock: AuthService):
+    user = await auth_service_mock.google_auth(code="fake_code")
+    decoded_user_id = auth_service_mock.get_user_id_from_access_token(access_token=user.access_token)
 
     assert user.user_id == decoded_user_id
     assert isinstance(user, UserLoginSchema)
 
-async def test_yandex_auth_success(auth_service: AuthService):
-    user = await auth_service.google_auth(code="fake_code")
-    decoded_user_id = auth_service.get_user_id_from_access_token(access_token=user.access_token)
+async def test_yandex_auth_success(auth_service_mock: AuthService):
+    user = await auth_service_mock.google_auth(code="fake_code")
+    decoded_user_id = auth_service_mock.get_user_id_from_access_token(access_token=user.access_token)
 
     assert user.user_id == decoded_user_id
     assert isinstance(user , UserLoginSchema)
